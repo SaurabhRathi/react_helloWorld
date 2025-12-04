4eb9da6fc18435cda92dfa072742e8102583fd6d
saurabh.rathi <saurabh.rathi@oracle.com>
2025-12-04 20:37:29 +0530

    First commit

---
+++ src/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Home</h1>
+      <p>Welcome to the Home page.</p>
+    </main>
+  );
+}
+
+export default Home;
